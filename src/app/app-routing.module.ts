c3ab7d8b26353590066269038f5dc9c73e9653e1
Wojciech Babicz <wojciech.babicz@onwelo.com>
2017-05-10 00:22:39 +0200

    Routing & Module changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+import { PageNotFoundComponent } from './not-found.component';
+import { CarsComponent } from './cars/cars.component';
+
+import { AuthGuard } from './auth-guard.service';
+import {AdminComponent} from './admin/admin.component';
+
+
+export const routes: Routes = [
+  {
+    path: 'cars',
+    component: CarsComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: '',   redirectTo: '', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PickerComponent } from './picker/picker.component';
 import { SharedModule } from './shared/shared.module';
+import { LoginRoutingModule } from './login/login-routing.module';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './not-found.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AppNavComponent,
-    PickerComponent
-  ],
   imports: [
     SharedModule,
     BrowserModule,
     CoreModule.forRoot(),
     CarsModule,
+    LoginRoutingModule,
     AppRoutingModule,
     NgbModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    AppNavComponent,
+    PickerComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    AdminComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
